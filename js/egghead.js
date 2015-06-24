3d2ba17ecc9ec0582eebe4ff2f99ed7d3b4db7fa
Steve Witman <stevewitman@gmail.com>
2015-06-23 19:10:24 -0600

    initial commit

---
+++ js/egghead.js
@@ -0,0 +1,52 @@
+var app = angular.module('egghead', ['firebase']);
+
+app.constant('FIREBASE_URI', 'https://brilliant-fire-2881.firebaseio.com/');
+
+app.controller('MainCtrl', ['$scope', 'ItemsService', function ($scope, ItemsService) {
+    $scope.newItem = { name: '', description: '', count: 0 };
+    $scope.currentItem = null;
+
+    $scope.items = ItemsService.getItems();
+
+    $scope.addItem = function () {
+        ItemsService.addItem(angular.copy($scope.newItem));
+        $scope.newItem = { name: '', description: '', count: 0 };
+    };
+
+    $scope.updateItem = function (id) {
+        ItemsService.updateItem(id);
+    };
+
+    $scope.removeItem = function (id) {
+        ItemsService.removeItem(id);
+    };
+}]);
+
+app.factory('ItemsService', ['$firebase', 'FIREBASE_URI', function ($firebase, FIREBASE_URI) {
+    var ref = new Firebase(FIREBASE_URI);
+    var items = $firebase(ref);
+
+
+    var getItems = function () {
+        return items;
+    };
+
+    var addItem = function (item) {
+        items.$add(item);
+    };
+
+    var updateItem = function (id) {
+        items.$save(id);
+    };
+
+    var removeItem = function (id) {
+        items.$remove(id);
+    };
+
+    return {
+        getItems: getItems,
+        addItem: addItem,
+        updateItem: updateItem,
+        removeItem: removeItem
+    }
+}]);
